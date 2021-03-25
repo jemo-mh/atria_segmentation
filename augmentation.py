a92b04d7c1dc60c74cb6fb0cdedb69fd0e2a4075
jm-ss <mhgjeamo@icloud.com>
2021-03-25 21:08:18 +0900

    fix augmentation

--- augmentation.py
+++ augmentation.py
@@ -2,50 +2,52 @@ import os
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import imgaug as ia
+from imgaug import augmenters as iaa
+import imageio
+
 img_path = '/home/gpu/Workspace/jm/left_atrial/dataset/img_data'
 label_path = '/home/gpu/Workspace/jm/left_atrial/dataset/label_data'
 img_list = os.listdir(img_path)
 label_list = os.listdir(label_path)
 
+# dl_img =[]
+# for i in label_list:
+#     image = Image.open(os.path.join(label_path, i))
+#     # print(image)
+#     zero = np.count_nonzero(image)
+#     if zero ==0:
+#         # print(i, zero)
+#         dl_img.append(i)
 
-print("number of original data",len(img_list), len(label_list))
-count=0
-count1=0
-count2=0
+# print("total img : ",len(img_list), "empty label : ", len(dl_img), "img to use : ",len(img_list)-len(dl_img))
+
+# count=0
+# for i in dl_img:
+#     dl_img_path = os.path.join(img_path, i)
+#     dl_label_path = os.path.join(label_path, i)
+#     if os.path.isfile(dl_img_path):
+#         os.remove(dl_img_path)
+#     if os.path.isfile(dl_label_path):
+#         os.remove(dl_label_path)
+
+# ia.seed(4)
 for i in range(len(img_list)):
-    image = Image.open(os.path.join(label_path,label_list[i]))
-    image= np.array(image)
-    non_zero = np.count_nonzero(image)
-    if non_zero ==0 :
-        # plt.imshow(image, cmap='gray')
-        # plt.show()
-        # print(image)
-        count+=1
-        dl_img_path = os.path.join(img_path, str(i)+'.png')
-        dl_label_path = os.path.join(label_path, str(i)+'.png')
-        if os.path.isfile(dl_img_path):
-            os.remove(dl_img_path)
-            count1+=1
-        if os.path.isfile(dl_label_path):
-            os.remove(dl_label_path)
-            count2+=1
+    image = imageio.imread(os.path.join(img_path, img_list[i]))
+    label = imageio.imread(os.path.join(label_path, label_list[i]))
+    # ia.imshow(image)
+    # print(image.shape, image.dtype)
 
+    rotate1 = iaa.Affine(rotate=(-10,-1))
+    rotate2 = iaa.Affine(rotate=(1,10))
+    img_aug1 = rotate2(image = image)
+    img_aug2 = rotate1(image=image)
+    label_aug1 = rotate1(image = label)
+    label_aug2 = rotate2(image= label)
+    print("Augmented:")
+    # ia.imshow(img_aug1)
+    # ia.imshow(img_aug2)
 
-        # print(dl_img_path, dl_label_path)
-
-print(count)
-print(count1, count2)
-
-img_path = '/home/gpu/Workspace/jm/left_atrial/dataset/img_data'
-label_path = '/home/gpu/Workspace/jm/left_atrial/dataset/label_data'
-img_list = os.listdir(img_path)
-label_list = os.listdir(label_path)
-
-
-print("number after deleting empty label data",len(img_list), len(label_list))
-count=0
-# path = '/home/gpu/Workspace/jm/left_atrial/dataset/label_data/3.png'
-# image = Image.open(path)
-# image= np.array(image)
-# plt.imshow(image, cmap='gray')
-# plt.show()
+    # ia.imshow(label)
+    # ia.imshow(label_aug1)
+    # ia.imshow(label_aug2)
